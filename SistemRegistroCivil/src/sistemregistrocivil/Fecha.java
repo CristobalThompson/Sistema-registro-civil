d5cc158bbba02e7100862ce102192561bbf4a419
Cristobal Thompson <cristo92014@gmail.com>
2025-08-31 22:39:08 -0400

    Clase fecha lista

--- SistemRegistroCivil/src/sistemregistrocivil/Fecha.java
+++ SistemRegistroCivil/src/sistemregistrocivil/Fecha.java
@@ -13,5 +13,39 @@ public class Fecha {
     private int dia;
     private int mes;
     private int año;
-    
+
+    // Día actual
+    int getDia() {
+        return dia;
+    }
+
+    // Mes actual
+    int getMes() {
+        return mes;
+    }
+
+    // Año actual
+    int getAño() {
+        return año;
+    }
+
+    // Cambia el día
+    void setDia(int nuevoDia) {
+        dia = nuevoDia;
+    }
+
+    // Cambia el mes
+    void setMes(int nuevoMes) {
+        mes = nuevoMes;
+    }
+
+    // Cambia el año
+    void setAño(int nuevoAño) {
+        año = nuevoAño;
+    }
+
+    // Muestra la fecha
+    void mostrarFecha() {
+        System.out.println(dia + "/" + mes + "/" + año);
+    }
 }
