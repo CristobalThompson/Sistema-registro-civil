fe1aa3e8766a4e785799568d3552c64dfc10600e
Cristobal Thompson <cristo92014@gmail.com>
2025-08-31 22:46:22 -0400

    Clase RegistroCivil lista

--- SistemRegistroCivil/src/sistemregistrocivil/RegistroCivil.java
+++ SistemRegistroCivil/src/sistemregistrocivil/RegistroCivil.java
@@ -13,6 +13,68 @@ package sistemregistrocivil;
 import java.util.*;
 
 public class RegistroCivil {
-    private HashMap sucursales;
-    private HashMap personas;
+    
+    private HashMap<String, Sucursal> sucursales = new HashMap<>();
+    private HashMap<String, Persona> listaPersonas = new HashMap<>();
+    
+
+    private boolean validarRut(String rut) {
+        return listaPersonas.containsKey(rut);
+    }
+    
+    public void agregarSucursal(String nombreSucursal, Sucursal sucursal) {
+    if (sucursales.containsKey(nombreSucursal)) {
+        System.out.println("La sucursal '" + nombreSucursal + "' ya existe.");
+    } else {
+        sucursales.put(nombreSucursal, sucursal);
+        System.out.println("Sucursal '" + nombreSucursal + "' agregada correctamente.");
+        }
+    }
+    
+    //Verificar nacimiento
+    public boolean nacimiento(Persona persona, String nombreSucursal) {
+        if (validarRut(persona.getRut())) {
+            System.out.println("El RUT ya está registrado.");
+            return false;
+        }
+
+        listaPersonas.put(persona.getRut(), persona);
+
+        Sucursal suc = sucursales.get(nombreSucursal);
+        if (suc != null) {
+            Archivo archivo = new Archivo();
+            boolean asignado = archivo.setPersona(persona);
+
+            if (!asignado) {
+                System.out.println("Error al asignar la persona al archivo.");
+                return false;
+            }
+
+            suc.agregarArchivo(nombreSucursal,archivo);
+        } else {
+            System.out.println("Sucursal no encontrada: " + nombreSucursal);
+            return false;
+        }
+
+        System.out.println("Nacimiento registrado en sucursal: " + nombreSucursal);
+        return true;
+    }
+
+    public Persona buscarPersona(String rut) {
+        return listaPersonas.get(rut);
+    }
+    //Mostrar los documentos del rut solicitado
+    public void mostrarDocumento(String rut) {
+        Persona persona = buscarPersona(rut);
+        if (persona != null) {
+            persona.mostrarDatos();
+        } else {
+            System.out.println("Persona no encontrada.");
+        }
+    }
+
+    public void leerDatos(Persona persona, String nombreSucursal) {
+        nacimiento(persona, nombreSucursal);
+    }
+
 }
