50a916bc7610a7b9bd47c67a941a1d7391a27615
Cristobal Thompson <cristo92014@gmail.com>
2025-08-31 22:41:26 -0400

    Clase Ubicacion lista

--- SistemRegistroCivil/src/sistemregistrocivil/Ubicacion.java
+++ SistemRegistroCivil/src/sistemregistrocivil/Ubicacion.java
@@ -14,4 +14,70 @@ public class Ubicacion {
     private String ciudad;
     private String comuna;
     private String direccion;
+
+    // Constructor
+    public Ubicacion(String region, String ciudad, String comuna, String direccion) {
+        this.region = region;
+        this.ciudad = ciudad;
+        this.comuna = comuna;
+        this.direccion = direccion;
+    }
+
+    //Mostrar Datos
+    public void Mostrar() {
+        System.out.println("Región: " + region);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Comuna: " + comuna);
+        System.out.println("Dirección: " + direccion);
+    }
+
+    // Getters
+    public String getRegion() {
+        return region;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    // Setters
+    public boolean setRegion(String region) {
+        if (region != null && !region.isEmpty()) {
+            this.region = region;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setCiudad(String ciudad) {
+        if (ciudad != null && !ciudad.isEmpty()) {
+            this.ciudad = ciudad;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setComuna(String comuna) {
+        if (comuna != null && !comuna.isEmpty()) {
+            this.comuna = comuna;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean setDireccion(String direccion) {
+        if (direccion != null && !direccion.isEmpty()) {
+            this.direccion = direccion;
+            return true;
+        }
+        return false;
+    }
 }
