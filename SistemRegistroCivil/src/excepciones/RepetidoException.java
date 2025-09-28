e20cd7f7500b8f62d365747d0d11ca26afee7983
Cristobal Thompson <cristo92014@gmail.com>
2025-09-28 04:26:46 -0300

    comentado exception repetido

--- SistemRegistroCivil/src/excepciones/RepetidoException.java
+++ SistemRegistroCivil/src/excepciones/RepetidoException.java
@@ -1,6 +1,9 @@
 package excepciones;
 
-
+/**
+ * Función destinada al dato repetido en el sistema
+ * @author franc
+ */
 public class RepetidoException extends Exception{
     public RepetidoException(){
         super("El elemento ya se encuentra registrado");
